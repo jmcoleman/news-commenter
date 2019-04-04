47cfd39daaf420fce6278d0187e610dde613b2fd
jmc <13514717+jmcoleman@users.noreply.github.com>
2019-04-03 21:05:17 -0400

    sticky footer

--- public/assets/routes/general-routes.js
+++ public/assets/routes/general-routes.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-// File Name: html-routes.js 
+// File Name: html-routes.js
 //
 // Description: This file offers a set of routes for sending users to the various html pages
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -12,197 +12,188 @@ const LOAD_FROM_SCRAPE = false;
 const SMASHING_MAGAZINE_URL = "https://www.smashingmagazine.com";
 
 module.exports = function(app) {
+	//////////////////////
+	// functions
+	//////////////////////
+	const scrapeSite = function(callback) {
+		// First, tell the console what server.js is doing
+		console.log(
+			"\n***********************************\n" +
+				"Grabbing every article name and link\n" +
+				"from Smashing Magazines's web site:" +
+				"\n***********************************\n"
+		);
 
-  //////////////////////
-  // functions
-  //////////////////////
-  const scrapeSite = function (callback) {
-    // First, tell the console what server.js is doing
-    console.log("\n***********************************\n" +
-                "Grabbing every article name and link\n" +
-                "from Smashing Magazines's web site:" +
-                "\n***********************************\n");
+		// Making a request for Smashing Magazines's web site for articles. The page's HTML is passed as the callback's third argument
+		request("https://www.smashingmagazine.com/articles/", function(
+			error,
+			response,
+			html
+		) {
+			if (error) {
+				return console.error("scape from site failed: ", error);
+			}
 
-    // Making a request for Smashing Magazines's web site for articles. The page's HTML is passed as the callback's third argument
-    request("https://www.smashingmagazine.com/articles/", function(error, response, html) {
+			// Load the HTML into cheerio and save it to a variable
+			// '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+			const $ = cheerio.load(html, { ignoreWhitespace: true });
 
-      if (error) {
-        return console.error('scape from site failed: ', error);
-      }
+			// An empty array to save the data that we'll scrape
+			let results = [];
 
-      // Load the HTML into cheerio and save it to a variable
-      // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-      const $ = cheerio.load(html, { ignoreWhitespace: true });
+			////////////////////////////////////////////////////////////////////////////////////////
+			// for each article
+			//   * Headline - the title of the article
+			//   * Summary - a short summary of the article
+			//   * URL - the url to the original article
+			//   * Feel free to add more content to your database (photos, bylines, and so on).
+			////////////////////////////////////////////////////////////////////////////////////////
 
-      // An empty array to save the data that we'll scrape
-      let results = [];
+			// With cheerio, find each "article--post" class
+			// (i: iterator. element: the current element)
+			$(".article--post").each(function(i, element) {
+				// Save the text of the "article--post__title" class element in a "headline" variable
+				var headline = $(element)
+					.find(".article--post__title")
+					.text();
 
-      ////////////////////////////////////////////////////////////////////////////////////////
-      // for each article
-      //   * Headline - the title of the article
-      //   * Summary - a short summary of the article
-      //   * URL - the url to the original article
-      //   * Feel free to add more content to your database (photos, bylines, and so on).
-      ////////////////////////////////////////////////////////////////////////////////////////
+				// In the currently selected element, look at its child elements (i.e., its p-tags with a class "article--post__teaser"),
+				// then filter for any text elements that are contents and save it to the "summary" variable
+				var summary = $(element)
+					.find("p.article--post__teaser")
+					.first()
+					.contents()
+					.filter(function() {
+						return this.type === "text";
+					})
+					.text();
 
-      // With cheerio, find each "article--post" class
-      // (i: iterator. element: the current element)
-      $(".article--post").each(function(i, element) {
+				// console.log("Stuff: " + $(element).find("div.author__image").children("div").data('alt'));
 
-          // Save the text of the "article--post__title" class element in a "headline" variable
-          var headline = $(element).find(".article--post__title").text();
+				// cheerio scrapes the html to get author, url and date of article
+				var author = $(element)
+					.find("div.author__image")
+					.children("div")
+					.data("alt");
+				var urlLink = $(element)
+					.find(".article--post__title")
+					.children()
+					.attr("href");
+				var date = $(element)
+					.find(".article--post__teaser time")
+					.attr("datetime");
 
-          // In the currently selected element, look at its child elements (i.e., its p-tags with a class "article--post__teaser"),
-          // then filter for any text elements that are contents and save it to the "summary" variable
-          var summary = $(element).find("p.article--post__teaser").first().contents().filter(function() {
-              return this.type === 'text';
-          }).text();
+				// If this found element had both a headline and a link
+				if (headline && urlLink) {
+					// save results in object and add to the results array
+					results.push({
+						headline: headline,
+						summary: summary.trim(),
+						urlLink: SMASHING_MAGAZINE_URL + urlLink,
+						author: author,
+						date: date
+					});
+				}
+			});
 
-          // console.log("Stuff: " + $(element).find("div.author__image").children("div").data('alt'));
+			// callback function with the result data returned from the scrape
+			callback(null, results);
+		});
+	};
 
-          // use cheerio to scrape the html and get the author, url and date of the article
-          var author = $(element).find("div.author__image").children("div").data('alt');
-          var urlLink = $(element).find(".article--post__title").children().attr("href");
-          var date = $(element).find(".article--post__teaser time").attr("datetime");
+	async function asyncForEach(array, callback) {
+		for (let index = 0; index < array.length; index++) {
+			await callback(array[index], index, array);
+		}
+	}
 
-          // If this found element had both a headline and a link
-          if (headline && urlLink) {
+	//////////////////////
+	// Routes
+	//////////////////////
 
-            // save the results in an object that we'll push into the results array that we defined earlier
-            results.push({
-              headline: headline,
-              summary: summary.trim(),
-              urlLink: SMASHING_MAGAZINE_URL + urlLink,
-              author: author,
-              date: date
-            });
+	// GET root route
+	app.get("/", function(req, res) {
+		// show all of the existing content along with the comments
+		res.redirect("/api/articles");
+	});
 
-            ////////////////////////
-            // push to Mongo DB
-            ////////////////////////
+	// GET scrape route to retrieve articles
+	app.get("/scrape", function(req, res) {
+		console.log("route: in scrape articles");
 
-            // console.log("route: scrape is creating article");
+		// scrapes and returns articles
+		scrapeSite(function(err, articleList) {
+			console.log("returned results in");
+			console.log(JSON.stringify(articleList));
 
-            // Save a new Example using the data object
-            // db.Article.create({
-            //   headline: headline,
-            //   summary: summary.trim(),
-            //   urlLink: SMASHING_MAGAZINE_URL + urlLink,
-            //   author: author,
-            //   date: date
-            // })
-            // .then(function(savedData) {
-            //   // if saved successfully
-            //   // console.log(savedData);
+			// for each item in the articleList, find it in Mongo and if it's not there, append it to a new array
+			// save the new ones to mongo
+			var newArticleList = [];
 
+			// articleList.forEach((item, index) => {
+			asyncForEach(articleList, async (item, index) => {
+				console.log("Finding index[" + index + "]: " + item.headline);
 
-            // })
-            // .catch(function(err) {
-            //   // If an error occurs, log the error message
-            //   console.log(err.message);
-            // });
-           
-          }
-      });
+				db.Article.find({ headline: item.headline.trim() })
+					.then(function(dbResult) {
+						console.log("Mongo has: ");
+						console.log(dbResult);
 
-      // log the results once you've looped through each of the elements found with cheerio
-      // console.log(results);
+						if (dbResult.length === 0) {
+							// if don't find it, we'll need to add it
+							console.log("add item to array to add to mongo.");
+							console.log(item);
+							newArticleList.push(item);
 
-      // call the callback function with the result data returned from the scrape
-      callback(null, results);
-    });
+							////////////////////////
+							// push to Mongo DB
+							////////////////////////
 
-  };
+							console.log("scrape is creating NEW article");
 
-  //////////////////////
-  // Routes 
-  //////////////////////
+							// Save a new Example using the data object
+							db.Article.create({
+								headline: item.headline.trim(),
+								summary: item.summary.trim(),
+								urlLink: item.urlLink, // website url has aleady been appended at this point
+								author: item.author,
+								date: item.date
+							})
+								.then(function(savedData) {
+									// if saved successfully
+									// console.log(savedData);
+								})
+								.catch(function(err) {
+									// If an error occurs, log the error message
+									console.log(err.message);
+								});
+						}
+					})
+					.catch(function(err) {
+						// If an error occurs, log the error message
+						console.log(err.message);
+					});
+			});
 
-  // GET root route
-  app.get("/", function(req, res) {
-    // show all of the existing content along with the comments
-    res.redirect("/api/articles");
-  });
+			// send the new article list to handlebars
+			var hbsObject = {
+				articles: newArticleList,
+				isScraping: true
+			};
+			res.render("index", hbsObject);
 
-  // GET scrape route to retrieve articles
-  app.get("/scrape", function(req, res) {
-    console.log("route: in scrape articles");
+			// END SCRAPESITE
+		});
+	});
 
-    // scrapes and returns articles
-    scrapeSite(function(err,articleList) {
-      console.log("returned results in");
-      console.log(JSON.stringify(articleList));
-  
-      // for each item in the articleList, find it in Mongo and if it's not there, append it to a new array
-      // save the new ones to mongo
-      var newArticleList = [];
+	// GET clear all articles (and their associated comments) route
+	app.get("/clear", function(req, res) {
+		console.log("route: in clear all articles");
 
-      articleList.forEach((item, index) => {
-        console.log("Finding: " + item.headline);
+		// clear all data
+		db.ArticleComment.deleteMany({}, function(err) {});
+		db.Article.deleteMany({}, function(err) {});
 
-        db.Article.find({headline: item.headline.trim()})
-          .then(function(dbResult){
-            console.log(dbResult);
-
-            if (dbResult.length === 0) {
-              // if don't find it, we'll need to add it
-              console.log(item);
-              newArticleList.push(item);
-
-            ////////////////////////
-            // push to Mongo DB
-            ////////////////////////
-
-            console.log("scrape is creating NEW article");
-
-            // Save a new Example using the data object
-            db.Article.create({
-              headline: item.headline,
-              summary: item.summary.trim(),
-              urlLink: item.urlLink,  // website url has aleady been appended at this point
-              author: item.author,
-              date: item.date
-            })
-            .then(function(savedData) {
-              // if saved successfully
-              // console.log(savedData);
-            })
-            .catch(function(err) {
-              // If an error occurs, log the error message
-              console.log(err.message);
-            });
-
-            }
-
-          })
-          .catch(function(err) {
-            // If an error occurs, log the error message
-            console.log(err.message);
-          });
-
-      });
-        
-      // send the new article list to handlebars
-      var hbsObject = {
-        articles: newArticleList,
-        isScraping: true
-      };
-      res.render("index", hbsObject);   
-    
-      // END SCRAPESITE
-    });
-
-  });
-  
-  // GET clear all articles (and their associated comments) route
-  app.get("/clear", function(req, res) {
-    console.log("route: in clear all articles");
-
-    // clear all data
-    db.ArticleComment.deleteMany({}, function(err) {});
-    db.Article.deleteMany({}, function(err) {});
-
-    res.redirect("/api/articles");
-  });
-
+		res.redirect("/api/articles");
+	});
 };
--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const mongoose = require("mongoose");
 /////////////////////////////////////////////////////////
 
 // use dotenv to read .env vars into Node but silence the Heroku log error for production as no .env will exist
-require('dotenv').config( { silent: process.env.NODE_ENV === 'production' } );
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 
 // process.env.NODE_ENV is set by heroku with a default value of production
-if (process.env.NODE_ENV === 'production') {
-  console.log("in PROD");
-  // connect to the MongoDB on heroku using MONGODB_URI below
+if (process.env.NODE_ENV === "production") {
+	console.log("in PROD");
+	// connect to the MongoDB on heroku using MONGODB_URI below
 } else {
-  console.log("in DEV");
-  // use the connection info from the .env file otherwise
-  require('dotenv').load();
+	console.log("in DEV");
+	// use the connection info from the .env file otherwise
+	require("dotenv").load();
 }
 // console.log("process env: " + JSON.stringify(process.env,null,'\t'));
 
@@ -22,10 +22,10 @@ if (process.env.NODE_ENV === 'production') {
 // dependencies
 //////////////////////////
 var express = require("express");
-var session = require('express-session');
-var path = require('path');
+var session = require("express-session");
+var path = require("path");
 var bodyParser = require("body-parser");
-var moment = require('moment');
+var moment = require("moment");
 
 ///////////////////////
 // configure Express
@@ -34,7 +34,7 @@ var app = express();
 app.use(express.json());
 
 // sets the port info
-app.set('port', (process.env.PORT || 8080));
+app.set("port", process.env.PORT || 8080);
 
 // body parsing middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,20 +42,21 @@ app.use(bodyParser.json());
 // app.use(cookieParser());
 
 // serve static folders
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/public/assets/img')); 
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/assets/img"));
 
 /////////////////////////
 // connect to Mongo DB
 /////////////////////////
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI =
+	process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true)
+mongoose.set("useCreateIndex", true);
 
 /////////////////
 // handlebars
@@ -65,37 +66,41 @@ var exphbs = require("express-handlebars");
 //Here you can pass helpers that you would normally define in registerHelpers
 //and you can also define stuff like `defaultLayout` and `partialsDir`
 var hbs = exphbs.create({
-  helpers: {
-      sayHello: function () { alert("Hello World") },
-      getStringifiedJson: function (value) {
-          return JSON.stringify(value);
-      },
-      readableDate: function(date) {
-        // return in utc to convert the date from the offset provided to UTC
-        // these dates have no timezone
-        return moment.utc(date).format('MM/DD/YYYY');
-      }
-  },
-  defaultLayout: 'main'
+	helpers: {
+		sayHello: function() {
+			alert("Hello World");
+		},
+		getStringifiedJson: function(value) {
+			return JSON.stringify(value);
+		},
+		readableDate: function(date) {
+			// return in utc to convert the date from the offset provided to UTC
+			// these dates have no timezone
+			return moment.utc(date).format("MM/DD/YYYY");
+		}
+	},
+	defaultLayout: "main"
 });
 
 // handlebar configuration
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 // more handlebars helpers
-require('handlebars-helpers')(['comparison']);
+require("handlebars-helpers")(["comparison"]);
 
 /////////////////////
 // Expess Session
 /////////////////////
 // console.log("secret: " +  process.env.SECRET_KEY);
-app.use(session({
-  secret: process.env.SECRET_KEY,     // put this in the heroku environment variables
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(
+	session({
+		secret: process.env.SECRET_KEY, // put this in the heroku environment variables
+		saveUninitialized: true,
+		resave: true
+	})
+);
 
 ////////////////////////////////////////////////////////
 // Import routes and give the server access to them.
@@ -103,6 +108,6 @@ app.use(session({
 var routes = require("./controllers/app_controller.js");
 app.use(routes);
 
-app.listen(app.get('port'), function() {
-    console.log("App now listening at localhost: " + app.get('port'));
+app.listen(app.get("port"), function() {
+	console.log("App now listening at localhost: " + app.get("port"));
 });
